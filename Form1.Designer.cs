d8963bfb779a4dd854fb4df70ddc348830a3e382
Ayoub Al Banna <ayoubbanna101@gmail.com>
2024-09-09 16:10:05 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PRODIGY_SD_04
+{
+    partial class SudokuSolver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolve.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.btnSolve.Location = new System.Drawing.Point(76, 326);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(113, 32);
+            this.btnSolve.TabIndex = 82;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblTitle.Location = new System.Drawing.Point(52, 280);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(212, 32);
+            this.lblTitle.TabIndex = 83;
+            this.lblTitle.Text = "Sudoku Solver";
+            // 
+            // SudokuSolver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Firebrick;
+            this.ClientSize = new System.Drawing.Size(383, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnSolve);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SudokuSolver";
+            this.Text = "SodukuSolver";
+            this.Load += new System.EventHandler(this.SudokuSolver_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using PRODIGY_SD_04;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRODIGY_SD_04
+{
+    public partial class SudokuSolver : Form
+    {
+        private TextBox[,] textBoxes = new TextBox[9,9];
+        public SudokuSolver()
+        {
+            InitializeComponent();
+            this.Text = "Sudoku Solver";
+            this.Width = 290;
+            this.Height = 400;
+
+            for (int i=0;i<9;i++)
+            {
+                for (int j=0;j<9;j++)
+                {
+                    textBoxes[i,j] = new TextBox
+                    {
+                        Width=30,
+                        Height=30,
+                        Font=new System.Drawing.Font("Arial",12),
+                        TextAlign=HorizontalAlignment.Center,
+                        Location=new System.Drawing.Point(i*30,j*30),                    
+                    };
+                    this.Controls.Add(textBoxes[i,j]);
+                }
+            }
+            
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            int[,] board = new int[9,9];
+            for (int i=0;i<9;i++)
+            {
+                for (int j=0;j<9;j++)
+                {
+                    if (textBoxes[i,j].Text!="")
+                    {
+                        board[i,j]=int.Parse(textBoxes[i,j].Text);
+                    }
+                }
+            }
+            if (Solve(board,0,0))
+            {
+                for (int i=0;i<9;i++)
+                {
+                    for (int j=0;j<9;j++)
+                    {
+                        textBoxes[i,j].Text=board[i,j].ToString();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No solution");
+            }
+        }
+
+        private bool IsValid(int[,] board,int row,int col,int num)
+        {
+            for (int x=0;x<9;x++)
+            {
+                if (board[row,x]==num)
+                {
+                    return false;
+                }
+            }
+
+            for (int x=0;x<9;x++)
+            {
+                if (board[x,col]==num)
+                {
+                    return false;
+                }
+            }
+
+            int startRow=row-row%3;
+            int startCol=col-col%3;
+            for (int i=0;i<3;i++)
+            {
+                for (int j=0;j<3;j++)
+                {
+                    if (board[i+startRow,j+startCol]==num)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool Solve(int[,] board,int row,int col)
+        {
+            if (row==9)
+            {
+                return true;
+            }
+
+            if (col==9)
+            {
+                return Solve(board,row+1,0);
+            }
+
+            if (board[row,col]!=0)
+            {
+                return Solve(board,row,col+1);
+            }
+
+            for (int num=1;num<=9;num++)
+            {
+                if (IsValid(board,row,col,num))
+                {
+                    board[row,col]=num;
+                    if (Solve(board,row,col+1))
+                    {
+                        return true;
+                    }
+                    board[row,col] = 0;
+                }
+            }
+            return false;
+        }
+        private void SudokuSolver_Load(object sender, EventArgs e) { }
+    }
+}
